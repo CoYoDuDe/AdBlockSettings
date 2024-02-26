faa461669e1c5fff1ef015349f0138ee03d97e4d
CoYoDuDe <157252929+CoYoDuDe@users.noreply.github.com>
2024-02-26 19:31:38 +0100

    Update blocklist_downloader.py

--- Scripts/blocklist_downloader.py
+++ Scripts/blocklist_downloader.py
@@ -10,13 +10,21 @@ def get_dbus_setting_value(path):
     bus = dbus.SystemBus()
     settings_service = bus.get_object("com.victronenergy.settings", path)
     settings_interface = dbus.Interface(settings_service, dbus_interface='com.victronenergy.BusItem')
-    return settings_interface.GetValue()
+    value = settings_interface.GetValue()
+    if isinstance(value, dbus.ByteArray):
+        return ''.join(format(byte, '02x') for byte in value)  # Konvertiert Byte-Array zu Hex-String
+    return value
 
 def set_dbus_setting_value(path, value):
     bus = dbus.SystemBus()
-    settings_service = bus.get_object("com.victronenergy.settings", "/")
-    settings_interface = dbus.Interface(settings_service, dbus_interface='com.victronenergy.BusItem')
-    settings_interface.SetValue(path, value)
+    settings_service = bus.get_object("com.victronenergy.settings", "/Settings")
+    settings_interface = dbus.Interface(settings_service, dbus_interface='com.victronenergy.Settings')
+    parts = path.rsplit('/', 1)
+    group, name = parts[0], parts[1]
+    # Konvertieren Sie den Hex-String zu einem Byte-Array für die Speicherung im D-Bus
+    byte_value = dbus.ByteArray(bytes.fromhex(value))
+    settings_interface.AddSetting(group, name, byte_value, 'ay', dbus.ByteArray([0]*len(byte_value)), dbus.ByteArray([255]*len(byte_value)))
+    print(f"MD5-Wert im D-Bus aktualisiert: {value}")
 
 def calculate_md5(content):
     hash_md5 = hashlib.md5()
@@ -29,19 +37,24 @@ def convert_to_dnsmasq_format(lines):
 def download_and_configure_ad_list():
     ad_list_url = get_dbus_setting_value("/Settings/AdBlock/AdListURL")
     response = requests.get(ad_list_url)
+    file_path = "/etc/dnsmasq.d/AdList.conf"
+    
+    # Überprüfen, ob die Datei existiert
+    file_exists = os.path.isfile(file_path)
+
     if response.status_code == 200:
         raw_content_md5 = calculate_md5(response.text)
         previous_raw_md5 = get_dbus_setting_value("/Settings/AdBlock/RawAdListMD5")
 
-        if raw_content_md5 != previous_raw_md5:
+        # Aktualisieren, wenn der MD5-Wert unterschiedlich ist ODER die Datei nicht existiert
+        if raw_content_md5 != previous_raw_md5 or not file_exists:
             lines = response.text.strip().split('\n')
             dnsmasq_lines = convert_to_dnsmasq_format(lines)
-            file_path = "/etc/dnsmasq.d/AdList.conf"
 
             with open(file_path, "w") as f:
                 f.write('\n'.join(dnsmasq_lines))
 
-            set_dbus_setting_value("/Settings/AdBlock/RawAdListMD5", dbus.ByteArray(raw_content_md5.encode('utf-8')))
+            set_dbus_setting_value("/Settings/AdBlock/RawAdListMD5", raw_content_md5)
 
             print("Ad-List successfully downloaded, converted, and updated.")
         else:

1b4f257fdf22af7dfce1753c6ef91d022dc79b30
CoYoDuDe <157252929+CoYoDuDe@users.noreply.github.com>
2024-02-26 21:54:38 +0100

    Update blocklist_downloader.py

--- Scripts/blocklist_downloader.py
+++ Scripts/blocklist_downloader.py
@@ -3,7 +3,6 @@
 
 import dbus
 import requests
-import hashlib
 import os
 
 def get_dbus_setting_value(path):
@@ -21,15 +20,9 @@ def set_dbus_setting_value(path, value):
     settings_interface = dbus.Interface(settings_service, dbus_interface='com.victronenergy.Settings')
     parts = path.rsplit('/', 1)
     group, name = parts[0], parts[1]
-    # Konvertieren Sie den Hex-String zu einem Byte-Array für die Speicherung im D-Bus
     byte_value = dbus.ByteArray(bytes.fromhex(value))
     settings_interface.AddSetting(group, name, byte_value, 'ay', dbus.ByteArray([0]*len(byte_value)), dbus.ByteArray([255]*len(byte_value)))
     print(f"MD5-Wert im D-Bus aktualisiert: {value}")
-
-def calculate_md5(content):
-    hash_md5 = hashlib.md5()
-    hash_md5.update(content.encode('utf-8'))
-    return hash_md5.hexdigest()
 
 def convert_to_dnsmasq_format(lines):
     return ["address=/{}/".format(line.split()[1]) for line in lines if line]
@@ -39,26 +32,14 @@ def download_and_configure_ad_list():
     response = requests.get(ad_list_url)
     file_path = "/etc/dnsmasq.d/AdList.conf"
     
-    # Überprüfen, ob die Datei existiert
-    file_exists = os.path.isfile(file_path)
-
     if response.status_code == 200:
-        raw_content_md5 = calculate_md5(response.text)
-        previous_raw_md5 = get_dbus_setting_value("/Settings/AdBlock/RawAdListMD5")
+        lines = response.text.strip().split('\n')
+        dnsmasq_lines = convert_to_dnsmasq_format(lines)
 
-        # Aktualisieren, wenn der MD5-Wert unterschiedlich ist ODER die Datei nicht existiert
-        if raw_content_md5 != previous_raw_md5 or not file_exists:
-            lines = response.text.strip().split('\n')
-            dnsmasq_lines = convert_to_dnsmasq_format(lines)
+        with open(file_path, "w") as f:
+            f.write('\n'.join(dnsmasq_lines))
 
-            with open(file_path, "w") as f:
-                f.write('\n'.join(dnsmasq_lines))
-
-            set_dbus_setting_value("/Settings/AdBlock/RawAdListMD5", raw_content_md5)
-
-            print("Ad-List successfully downloaded, converted, and updated.")
-        else:
-            print("No changes detected in Ad-List.")
+        print("Ad-List successfully downloaded and updated.")
     else:
         print("Download failed with status code:", response.status_code)
 
